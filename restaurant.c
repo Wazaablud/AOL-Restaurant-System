37439a3393a458557d8ce6a2e5d3df4b8c472c31
Nevanblud <nevan.athallahyumna@binus.ac.id>
2025-12-27 15:31:17 +0700

    Change total type from double to float

--- restaurant.c
+++ restaurant.c
@@ -3,13 +3,13 @@
 
 struct Customer {
 	char name[50];
-	double total;
+	float total;
 };
 
-void savePurchase(char name[], double total) {
+void savePurchase(char name[], float total) {
 	FILE *fp = fopen("Top Spender.txt", "a");
     if (fp == NULL) return;
-    fprintf(fp, "%s %.2lf\n", name, total);
+    fprintf(fp, "%s %.2f\n", name, total);
     fclose(fp);
 }
 
@@ -23,24 +23,24 @@ void displaySpender() {
         return;
     }
 
-    while (fscanf(fp, "%s %.2lf", customers[count].name, &customers[count].total) != EOF) {
-        count ++;
+    while (count < 100 && fscanf(fp, "%s %f", customers[count].name, &customers[count].total) == 2) {
+        count++;
     }
     fclose(fp);
 
     for (int i = 0; i < count - 1; i++) {
         for (int j = 0; j < count - i - 1; j++) {
-            if (customers[j].total < customers[j+1].total) {
+            if (customers[j].total < customers[j + 1].total) {
                 struct Customer temp = customers[j];
-                customers[j] = customers[j+1];
-                customers[j+1] = temp;
+                customers[j] = customers[j + 1];
+                customers[j + 1] = temp;
             }
         }
     }
 
     printf("\n--- TOP SPENDER ---\n");
     for (int i = 0; i < count; i++) {
-        printf("%d. %s - Rp%.2lf\n", i + 1, customers[i].name, customers[i].total);
+        printf("%d. %s - Rp%.2f\n", i + 1, customers[i].name, customers[i].total);
     }
 }
 
@@ -51,9 +51,9 @@ void searchCustomer(char searchName[]) {
 
     if (fp == NULL) return;
 
-    while (fscanf(fp, "%s %.2lf", p.name, &p.total) != EOF) {
+    while (fscanf(fp, "%s %.2f", p.name, &p.total) != EOF) {
         if (strcmp(p.name, searchName) == 0) {
-            printf("Customer found: %s has total spend of Rp%.2lf\n", p.name, p.total);
+            printf("Customer found: %s has total spend of Rp%.2f\n", p.name, p.total);
             found = 1;
         }
     }
@@ -63,7 +63,7 @@ void searchCustomer(char searchName[]) {
 
 int main() {
 	int mainMenu, choice, quantity, z, keepOrdering = 1;
-	double total1, total2, total3, total4, currentTotal = 0;
+	float total1, total2, total3, total4, currentTotal = 0;
 	char more;
 	char customerName[50];
 	char itemName[4][10];
@@ -80,6 +80,7 @@ int main() {
 		scanf("%d", &mainMenu);
 
 		if (mainMenu == 1) {
+			total1 = total2 = total3 = total4 = 0;
 			printf("Please enter your name: ");
 			scanf("%s", customerName);
 			printf("Hello, %s\n", customerName);
@@ -104,10 +105,10 @@ int main() {
     			scanf("%d", &quantity);
 
     			switch (choice){
-    				case 1: total1 = 20000 * quantity; break;
-					case 2: total2 = 35000 * quantity; break;
-					case 3: total3 = 15000 * quantity; break;
-					case 4: total4 = 10000 * quantity; break;
+    				case 1: total1 += 20000 * quantity; break;
+					case 2: total2 += 35000 * quantity; break;
+					case 3: total3 += 15000 * quantity; break;
+					case 4: total4 += 10000 * quantity; break;
 					default:
 					printf("Invalid Choice!\n");
     			}
@@ -117,7 +118,7 @@ int main() {
     			currentTotal = total1 + total2 + total3 + total4;
 			} while (more == 'y' || more == 'Y');
 
-			printf("Your total is Rp%.2lf", currentTotal);
+			printf("Your total is Rp%.2f", currentTotal);
 			savePurchase(customerName, currentTotal);
 			keepOrdering = 0;
 		}
@@ -134,6 +135,10 @@ int main() {
 		}
 		else if (mainMenu == 4) {
 			return 0;
+		}
+		else {
+			printf("Invalid input!\n");
+			keepOrdering = 0;
 		}
 	}
   
